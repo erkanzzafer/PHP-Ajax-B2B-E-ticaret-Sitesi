b0246d59bfbdc399d323d0d31244f1f7b82abcf5
erkanzzafer <zzafererkann@gmail.com>
2024-02-22 20:42:50 +0300

    tema parçalanması tamamlandı

---
+++ inc/footer.php
@@ -0,0 +1,173 @@
+<!-- FOOTER START -->
+<footer>
+		<!-- Footer-area start -->
+		<div class="footer-area footer-2">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4 col-md-6">
+						<div class="single-footer">
+							<h3 class="footer-title  title-border">Contact Us</h3>
+							<ul class="footer-contact">
+								<li><span>Address :</span>28 Green Tower, Street Name,<br>New York City, USA</li>
+								<li><span>Cell-Phone :</span>012345 - 123456789</li>
+								<li><span>Email :</span>your-email@gmail.com</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-3 col-sm-6">
+						<div class="single-footer">
+							<h3 class="footer-title  title-border">accounts</h3>
+							<ul class="footer-menu">
+								<li><a href="my-account.html"><i class="zmdi zmdi-dot-circle"></i>My Account</a></li>
+								<li><a href="wishlist.html"><i class="zmdi zmdi-dot-circle"></i>My Wishlist</a></li>
+								<li><a href="cart.html"><i class="zmdi zmdi-dot-circle"></i>My Cart</a></li>
+								<li><a href="login.html"><i class="zmdi zmdi-dot-circle"></i>Sign In</a></li>
+								<li><a href="checkout.html"><i class="zmdi zmdi-dot-circle"></i>Check out</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-3 col-sm-6">
+						<div class="single-footer">
+							<h3 class="footer-title  title-border">shipping</h3>
+							<ul class="footer-menu">
+								<li><a href="#"><i class="zmdi zmdi-dot-circle"></i>New Products</a></li>
+								<li><a href="#"><i class="zmdi zmdi-dot-circle"></i>Top Sellers</a></li>
+								<li><a href="#"><i class="zmdi zmdi-dot-circle"></i>Manufactirers</a></li>
+								<li><a href="#"><i class="zmdi zmdi-dot-circle"></i>Suppliers</a></li>
+								<li><a href="#"><i class="zmdi zmdi-dot-circle"></i>Specials</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6">
+						<div class="single-footer newsletter-item">
+							<h3 class="footer-title  title-border">Email Newsletters</h3>
+							<div class="footer-subscribe">
+								<form action="#">
+									<input type="text" name="email" placeholder="Email Address..." />
+									<button class="button-one submit-btn-4" type="submit" data-text="Subscribe">Subscribe</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Footer-area end -->
+		<!-- Copyright-area start -->
+		<div class="copyright-area copyright-2">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="copyright">
+							<p class="mb-0">&copy; <a href="https://themeforest.net/user/codecarnival/portfolio" target="_blank">CodeCarnival </a> 2021. All Rights Reserved.</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="payment  text-md-end">
+							<a href="#"><img src="img/payment/1.png" alt="" /></a>
+							<a href="#"><img src="img/payment/2.png" alt="" /></a>
+							<a href="#"><img src="img/payment/3.png" alt="" /></a>
+							<a href="#"><img src="img/payment/4.png" alt="" /></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Copyright-area start -->
+	</footer>
+	<!-- FOOTER END -->
+	<!-- QUICKVIEW PRODUCT -->
+	<div id="quickview-wrapper">
+		<!-- Modal -->
+		<div class="modal fade" id="productModal" tabindex="-1" role="dialog">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					</div>
+					<div class="modal-body">
+						<div class="modal-product">
+							<div class="product-images">
+								<div class="main-image images">
+									<img alt="#" src="img/product/quickview-photo.jpg" />
+								</div>
+							</div><!-- .product-images -->
+
+							<div class="product-info">
+								<h1>Aenean eu tristique</h1>
+								<div class="price-box-3">
+									<hr />
+									<div class="s-price-box">
+										<span class="new-price">$160.00</span>
+										<span class="old-price">$190.00</span>
+									</div>
+									<hr />
+								</div>
+								<a href="shop.html" class="see-all">See all features</a>
+								<div class="quick-add-to-cart">
+									<form method="post" class="cart">
+										<div class="numbers-row">
+											<input type="number" id="french-hens" value="3" min="1">
+										</div>
+										<button class="single_add_to_cart_button" type="submit">Add to cart</button>
+									</form>
+								</div>
+								<div class="quick-desc">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla augue nec est tristique auctor. Donec non est at libero.
+								</div>
+								<div class="social-sharing">
+									<div class="widget widget_socialsharing_widget">
+										<h3 class="widget-title-modal">Share this product</h3>
+										<ul class="social-icons">
+											<li><a target="_blank" title="Google +" href="#" class="gplus social-icon"><i class="zmdi zmdi-google-plus"></i></a></li>
+											<li><a target="_blank" title="Twitter" href="#" class="twitter social-icon"><i class="zmdi zmdi-twitter"></i></a></li>
+											<li><a target="_blank" title="Facebook" href="#" class="facebook social-icon"><i class="zmdi zmdi-facebook"></i></a></li>
+											<li><a target="_blank" title="LinkedIn" href="#" class="linkedin social-icon"><i class="zmdi zmdi-linkedin"></i></a></li>
+										</ul>
+									</div>
+								</div>
+							</div><!-- .product-info -->
+						</div><!-- .modal-product -->
+					</div><!-- .modal-body -->
+				</div><!-- .modal-content -->
+			</div><!-- .modal-dialog -->
+		</div>
+		<!-- END Modal -->
+	</div>
+	<!-- END QUICKVIEW PRODUCT -->
+
+</div>
+<!-- WRAPPER END -->
+
+<!-- all js here -->
+<!-- jquery latest version -->
+<script src="js/vendor/jquery-3.6.0.min.js"></script>
+<script src="js/vendor/jquery-migrate-3.3.2.min.js"></script>
+<!-- bootstrap js -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<!-- jquery.meanmenu js -->
+<script src="js/jquery.meanmenu.js"></script>
+<!-- slick.min js -->
+<script src="js/slick.min.js"></script>
+<!-- jquery.treeview js -->
+<script src="js/jquery.treeview.js"></script>
+<!-- lightbox.min js -->
+<script src="js/lightbox.min.js"></script>
+<!-- jquery-ui js -->
+<script src="js/jquery-ui.min.js"></script>
+<!-- jquery.nivo.slider js -->
+<script src="lib/js/jquery.nivo.slider.js"></script>
+<script src="lib/home.js"></script>
+<!-- jquery.nicescroll.min js -->
+<script src="js/jquery.nicescroll.min.js"></script>
+<!-- countdon.min js -->
+<script src="js/countdon.min.js"></script>
+<!-- wow js -->
+<script src="js/wow.min.js"></script>
+<!-- plugins js -->
+<script src="js/plugins.js"></script>
+<!-- main js -->
+<script src="js/main.js"></script>
+</body>
+
+</html>
